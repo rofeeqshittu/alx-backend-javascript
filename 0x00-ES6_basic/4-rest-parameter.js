94ea93f79312fd4f01fe148f8f6fc0a56ddb8cbc
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-10-01 22:19:16 +0100

    rest parameter syntax for fxn

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,7 @@
 export default function returnHowManyArguments(...Args) {
-    let total = 0;
-    for (const arg of Args) {
-	    total += 1;
-    }
-    return total;
+  let total = 0;
+  for (const arg of Args) {
+    total += 1;
+  }
+  return total;
 }
