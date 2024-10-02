11de2acc31721376e16a88ef3027e3f12c91bfdb
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-10-02 12:33:34 +0100

    iterate through objects

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,8 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const employeeNames = [];
+  
+  for (const employee of reportWithIterator) {
+    employeeNames.push(employee);
+  };
+  return employeeNames.join(' | '); // Join the names with ' | ' as seperator.
+}
