dd2dd8ae37b126388f02c8e2d392a44ba3002c4f
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-10-04 00:18:23 +0100

    Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+// Return a promise using prototype
+
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+     // The promise is created, but not resolved or rejected yet
+  });
+}
+
+export default getResponseFromAPI;
