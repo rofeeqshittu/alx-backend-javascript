d4834f43c511a7f3ae587af12600852db4fcf0c5
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-10-05 00:19:08 +0100

    Handle multiple promises - '.allsettled'

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from './4-user-promise.js'
-import uploadPhoto from './5-photo-reject.js'
+import signUpUser from './4-user-promise'
+import uploadPhoto from './5-photo-reject'
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
@@ -8,7 +8,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
       // Format the results as expected
       results.map(result => ({
         status: result.status,
-        value: result.value || String(result.reason), // 'value' if fulfilled, 'reason' if not
+        value: result.status === 'fulfilled' ?  result.value : String(result.reason),
       }))
     });
 }
