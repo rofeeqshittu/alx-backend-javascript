2a72311aa6d813f93cbb47474c1a811723656946
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-10-05 00:48:30 +0100

    Throw an err

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(String(error));
+  }
+
+  queue.push('Guardrail was processed');
+  return queue;
+}
