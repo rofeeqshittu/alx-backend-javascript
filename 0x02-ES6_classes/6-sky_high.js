44e4dad812e06589241b40e22fae77ffb2bc5b68
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-10-07 14:48:58 +0100

    Inheritance

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -16,6 +16,6 @@ export default class SkyHighBuilding extends Building {
 
   // Overide the evacuationWarningMessage method
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
