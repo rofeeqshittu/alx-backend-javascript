10516902c09b48f49c1d8b9e350f6fb2be2aef40
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-10-07 14:31:11 +0100

    A Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+
+    // Check if the subclass implements evacuationWarningMessage
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
