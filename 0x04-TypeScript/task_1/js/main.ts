6f8a78c5ed8df9c5791f03d9ef08227fc43f8c49
rofeeqshittu <rofeeqshittu21@gmail.com>
2024-10-09 14:52:25 +0100

    writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,3 +11,37 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+interface printTeacherFxn {
+  (firstName: string; lastName: string): string
+}
+
+const printTeacher: printTeacherFxn = (firstName: string, lastName: string): string => {
+  return `${firstName.charAt(0)}. ${lastName}`; // firstName starting letter . lastName
+}
+
+
+// Interface for the constructor for  StudentClass
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+// Interface describing the methods of the StudentClass
+interface StudentClassInterface {
+  workOnHome(): string;
+  displayName(): string;
+}
+
+// Implementing the StudentClass
+class StudentClass implements StudentClassInterface {
+  constructor(public firstName: string, public lastName: string) {}
+
+  // Method that returns a string indicating work is in progress
+  workOnHome(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
